5805ffcf2e5754ec23f79edb0def514b2610491d
Anderson Lopes <anderson.lopes.2010@gmail.com>
2020-08-19 20:46:42 +0200

    Creates project.

---
+++ src/main/java/com/lopes/mytravelsapi/MyTravelsApiApplication.java
@@ -0,0 +1,13 @@
+package com.lopes.mytravelsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTravelsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTravelsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lopes/mytravelsapi/MyTravelsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopes.mytravelsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTravelsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
